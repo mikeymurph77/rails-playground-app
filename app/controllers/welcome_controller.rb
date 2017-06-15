dfa33ecc64c326221952cdb2865b7d4b0fcaa0f6
Michael Murphy <mmurphy@jobcase.com>
2017-06-15 11:17:05 -0400

    Check playlist ownership
    Get a list of playlists the user owns

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,32 @@ class WelcomeController < ApplicationController
     if session[:spotify_user]
       # me = RSpotify::User.find(cookies[:spotify_user])
       spotify_user = RSpotify::User.new(session[:spotify_user])
-      binding.pry
+
+      # Get count of User's total playlists
+      count = 0
+      limit = 50
+      offset = 0
+      total_num_in_pass = 50
+      owners_playlists = []
+
+      while total_num_in_pass == 50 do
+        playlist_results = spotify_user.playlists(limit: 50, offset: offset)
+        total_num_in_pass = playlist_results.size
+
+        playlist_results.each do |playlist|
+          if playlist.owner.id == spotify_user.id
+            owners_playlists << playlist.id
+          end
+        end
+
+        count += total_num_in_pass
+        offset += 50
+      end
+
+      p "total playlists: #{count}"
+      p "total OWNED playlists: #{owners_playlists.size}"
+      p "total FOLLOWING playlists: #{count - owners_playlists.size}"
+      p owners_playlists
     end
   end
 end
